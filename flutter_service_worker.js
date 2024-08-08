6a281e952a9507d337e504579036b28f55073802
rahmadirsp <rahmadi.rr@gmail.com>
2024-08-08 07:17:40 +0700

    fixed the sign in button overflow

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,11 +29,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "65af23579fd72d8860e31fe11b6a54a1
 "icons/Icon-512.png": "b81b3e95c24138c6b02809c0a139d7e4",
 "icons/Icon-maskable-192.png": "ad869980fd42053640328c57c1a2fc26",
 "icons/Icon-maskable-512.png": "b81b3e95c24138c6b02809c0a139d7e4",
-"index.html": "40093c085a4de1404ce85be5b9d58d5e",
-"/": "40093c085a4de1404ce85be5b9d58d5e",
-"main.dart.js": "f22c0fd20131437fe16c95da9e705709",
+"index.html": "b7d6c3c795911f8070a0d9b3a415f086",
+"/": "b7d6c3c795911f8070a0d9b3a415f086",
+"main.dart.js": "ebfc6bbd499259d0a2217b92c5825a4f",
 "manifest.json": "ceca599daff5a80c0f81304dc72fd8bc",
-"version.json": "87d296ffa1e067800fcb74daec78ab4b"};
+"version.json": "f49de3f99224a3bff51c8e38a8c9001c"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
