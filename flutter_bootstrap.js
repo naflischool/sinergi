fcf60c15cfbf0d2cf5a95e92663becee7c6a1d6f
rahmadirsp <rahmadi.rr@gmail.com>
2025-10-06 12:42:09 +0700

    change using static image, and url forwarder

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2471660978"
+    serviceWorkerVersion: "650216232"
   }
 });
